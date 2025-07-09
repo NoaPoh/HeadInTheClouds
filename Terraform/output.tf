06ea6885abffb5efbb75b3189e84bcf19dccccae
johnny <324825470@idf.il>
2025-07-09 19:48:43 +0300

    format tf files

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,19 +1,19 @@
 output "AMI-ID" {
-  value       = data.aws_ami.amzlinux.id
+  value       = data.aws_ami.last-amazon-linux.id
   description = "The AMI ID"
 }
 
 output "AMI-Name" {
-  value       = data.aws_ami.amzlinux.name
+  value       = data.aws_ami.last-amazon-linux.name
   description = "The AMI Name"
 }
 
 output "public_ip" {
-  value       = aws_instance.colman-demo.public_ip
+  value       = aws_instance.sheleg-instance.public_ip
   description = "The public IP of the web server"
 }
 
 output "elb_dns_name" {
-  value       = aws_lb.myalb.dns_name
+  value       = aws_lb.sheleg-load-balancer.dns_name
   description = "The domain name of the load balancer"
 }
