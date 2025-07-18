4e0bb4d992733a531676348d7e37df514ce3ee21
johnny <324825470@idf.il>
2025-07-18 18:20:56 +0300

    fix: update HTTP_PORT in .env and correct setup-server.sh script structure

--- Terraform/setup-server.sh
+++ Terraform/setup-server.sh
@@ -3,15 +3,15 @@ echo "User data started at $(date)" >> /var/log/user-data-custom.log 2>&1
 yum update -y
 curl -fsSL https://rpm.nodesource.com/setup_22.x | bash -
 yum install -y nodejs git unzip
+sudo npm install pm2@latest -g
 git clone https://yehonatan930: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@github.com/NoaPoh/HeadInTheClouds.git
 cd HeadInTheClouds
-npm install pm2@latest -g
-cd client
+cd frontend
 npm i
-npm npm run build
-rm -rf ../server/build
-mv ./build ../server
-cd ../server
+npm run build
+rm -rf ../backend/build
+mv ./build ../backend
+cd ../backend
 npm i
 npm run prod
 echo "User data finished at $(date)" >> /var/log/user-data-custom.log 2>&1
