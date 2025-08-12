3c81f17578dd35dfe8978fae5d1ba119fd794ebc
johnny <324825470@idf.il>
2025-08-12 22:06:55 +0300

    trying

--- Terraform/setup-server.sh
+++ Terraform/setup-server.sh
@@ -2,19 +2,23 @@
 echo "User data started at $(date)" >> /var/log/user-data-custom.log 2>&1
 yum update -y
 curl -fsSL https://rpm.nodesource.com/setup_22.x | bash -
-yum install -y nodejs git unzip
+yum install -y nodejs nginx git unzip
 sudo npm install pm2@latest -g
+npm i -g http-server
 git clone https://yehonatan930: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@github.com/NoaPoh/HeadInTheClouds.git
 sudo fallocate -l 2G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 cd HeadInTheClouds
-cd backend
+cd Terraform
+sudo cp nginx.conf /etc/nginx/conf.d/myapp.conf
+sudo nginx -t
+sudo systemctl restart nginx
+cd ../backend
 npm i
 npm run prod
 cd ../frontend
-npm i -g http-server
 npm i
 rm -rf node_modules/.vite
 NODE_OPTIONS="--max-old-space-size=2048" npm run build
