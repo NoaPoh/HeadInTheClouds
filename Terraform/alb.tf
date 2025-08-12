b700176efd2a9734f3f43658fc59a2674fd4bfc6
נועה <noa852002@gmail.com>
2025-08-12 21:38:50 +0300

    add backend target group

--- Terraform/alb.tf
+++ Terraform/alb.tf
@@ -39,6 +39,14 @@ resource "aws_lb_listener" "http" {
   }
 }
 
+resource "aws_lb_target_group" "backend" {
+  name        = "backend"
+  target_type = "instance"
+  port        = 3010
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+}
+
 # resource "aws_lb_target_group_attachment" "sheleg-attachment" {
 #   target_group_arn = aws_lb_target_group.http.arn
 #   target_id        = aws_instance.sheleg-instance.id
