865c041fc7cc9a9bc866e50d53412012d62e3448
johnny <324825470@idf.il>
2026-01-15 19:13:01 +0200

    Refactor code structure for improved readability and maintainability

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -2,8 +2,6 @@
 provider "aws" {
   # profile    = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
   region     = "eu-central-1"
-  access_key = "AKIA5FHE3ZLIR6ARBQPH"
-  secret_key = "atxOCIxxjSIQS14tybYSmjM5x7XTOrMecO/ZwHZA"
 }
 
 data "aws_key_pair" "jivana_secret_key" {
